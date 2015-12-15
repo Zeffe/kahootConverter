07e50ce420a24d81681a09b4c5935349fae52c8a
Zeffer <zeffer5789@gmail.com>
2015-12-15 00:09:43 -0600

    Reads first question on page to a string

--- base.py
+++ base.py
@@ -1,6 +1,26 @@
+import urllib
 
 class Base():
 	def __init__(self):
 		print "Kahoot Converter v1"
 		print "\"What a shitty name\""
 base = Base()
+
+try:
+	url = raw_input("URL:")
+	
+	url2 = urllib.urlopen(url)
+
+	data = url2.read()
+
+	url2.close()
+	
+	newData = data.split("<span class='TermText qDef lang-en'>")[1].split("</span>")[0]
+except:
+	print ("Invalid URL")
+	raw_input()
+	exit()
+
+print newData
+
+raw_input()
