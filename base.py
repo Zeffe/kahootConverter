b9d742783ae7fefd33e9502d919c767129a3b388
Zeffer <zeffer5789@gmail.com>
2015-12-15 23:04:40 -0600

    Fixed output more

--- base.py
+++ base.py
@@ -4,14 +4,14 @@ class Base():
         def __init__(self):
                 print "Kahoot Converter v1"
                 print "\"What a shitty name\""
-		self.url = {"testURL":"https://quizlet.com/110838121/test"}
+                self.url = {"testURL":"https://quizlet.com/110838121/test?mult_choice=on&prompt-with=1&limit=40"}
 base = Base()
 
 try:
         # Get URL from user input.
         #url = raw_input("URL:")
-	url = base.url["testURL"]
-	
+        url = base.url["testURL"]
+        
         # Open the given URL on url2.
         url2 = urllib.urlopen(url)
 
@@ -20,32 +20,19 @@ try:
 
         # Close the url.
         url2.close()
-
-        # Split the question data into an array of strings.
-        qData = data.split("<span class='TermText qDef lang-en'>")[1].split("</span>")[0]
-                        
-        # Split the answer data into an array of strings.
-        aData = data.split("<span class='TermText qWord lang-en'>")[1].split("<\/span>")[0]
 except:
         # Exit if an error occurs.
         print ("Invalid URL")
         raw_input()
         exit()
 
-print data
-raw_input()
-nudata = data.split("<span class='TermText qWord lang-en'>")
-for i in range(3, 50):	
-	print "[" + str(i) + "]" + nudata[i].split("</span>")[0]
-raw_input()
+aData = data.split("<span class='TermText qWord lang-en'>")
+qData = data.split("<span class='TermText qDef lang-en'>")
 
-while True:
-        i = raw_input("Question:")
-        print ""
-        print data.split("<span class='TermText qDef lang-en'>")[int(i)].split("</span>")[0]
-        print ""
-        print data.split("<span class='TermText qWord lang-en'>")[int(i)].split("<\/span>")[0]
-        print ""
-        
-
+count = 0
+for i in range(1, len(qData)):
+        print "[Q" + str(i) + "]" + qData[i].split("</span>")[0]
+        for j in range(1, 5):
+                count += 1
+                print "[A" + str(j) + "]" + aData[i + count].split("</span>")[0]
 raw_input()
