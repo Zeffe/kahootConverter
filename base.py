3d993746c7d77a9e5fc84b268a3ea4628f18e82a
Zeffer <zeffer5789@gmail.com>
2015-12-15 21:38:56 -0600

    Changed output

--- base.py
+++ base.py
@@ -1,37 +1,47 @@
 import urllib
 
 class Base():
-	def __init__(self):
-		print "Kahoot Converter v1"
-		print "\"What a shitty name\""
+        def __init__(self):
+                print "Kahoot Converter v1"
+                print "\"What a shitty name\""
 base = Base()
 
 try:
         # Get URL from user input.
-	url = raw_input("URL:")
+        url = raw_input("URL:")
 
-	# Open the given URL on url2.
-	url2 = urllib.urlopen(url)
+        # Open the given URL on url2.
+        url2 = urllib.urlopen(url)
 
         # Read the data from the url to 'data'
-	data = url2.read()
+        data = url2.read()
 
         # Close the url.
-	url2.close()
+        url2.close()
 
-	# Split the question data into an array of strings.
-	qData = data.split("<span class='TermText qDef lang-en'>")[1].split("</span>")[0]
-	# Split the answer data into an array of strings.
-	aData = data.split("<span class='TermText qWord lang-en'>")[1].split("<\/span>")[0]
+        # Split the question data into an array of strings.
+        qData = data.split("<span class='TermText qDef lang-en'>")[1].split("</span>")[0]
+                        
+        # Split the answer data into an array of strings.
+        aData = data.split("<span class='TermText qWord lang-en'>")[1].split("<\/span>")[0]
 except:
         # Exit if an error occurs.
-	print ("Invalid URL")
-	raw_input()
-	exit()
+        print ("Invalid URL")
+        raw_input()
+        exit()
 
-# Display question and answer data.
-print qData
-print ""
-print aData
+nudata = data.split("qWord")
+for i in nudata:
+        print i
+raw_input()
+
+while True:
+        i = raw_input("Question:")
+        print ""
+        print data.split("<span class='TermText qDef lang-en'>")[int(i)].split("</span>")[0]
+        print ""
+        print data.split("<span class='TermText qWord lang-en'>")[int(i)].split("<\/span>")[0]
+        print ""
+        
 
 raw_input()
